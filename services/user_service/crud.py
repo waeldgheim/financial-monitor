d3264cdf90081356a7668eb9390901ee109f770a
waeldgheim <waeldgheim@hotmail.com>
2025-10-25 18:54:55 +0300

    Created authenticate user function

--- services/user_service/crud.py
+++ services/user_service/crud.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 from models import User
-from utils import hash_password
+from utils import hash_password, verify_password
 
 def create_user(db: Session, username: str, email: str, password: str):
     existing_user = db.query(User).filter(User.email == email).first()
@@ -12,3 +12,11 @@ def create_user(db: Session, username: str, email: str, password: str):
     db.commit()
     db.refresh(db_user)
     return db_user
+
+def authenticate_user(db: Session, email: str, password: str):
+    user = db.query(User).filter(User.email == email).first()
+    if not user:
+        return None
+    if not verify_password(password, user.hashed_password):
+        return None
+    return user
