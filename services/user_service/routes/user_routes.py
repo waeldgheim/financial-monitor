0678536d120336aa8edef19f6e831150163d9d1c
waeldgheim <waeldgheim@hotmail.com>
2025-10-25 18:55:35 +0300

    Created login endpoint

--- services/user_service/routes/user_routes.py
+++ services/user_service/routes/user_routes.py
@@ -2,8 +2,11 @@ from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 from shared_lib.db import SessionLocal
 from models import User
-from crud import create_user
-from schemas import UserCreate, UserResponse
+from crud import create_user, authenticate_user
+from schemas import UserCreate, TokenResponse, LoginRequest
+from utils import create_access_token
+from datetime import timedelta
+
 
 
 router = APIRouter()
@@ -20,7 +23,7 @@ def get_db():
 def read_users(db: Session = Depends(get_db)):
     return db.query(User).all()
 
-@router.post("/register", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
+@router.post("/register", response_model=TokenResponse, status_code=status.HTTP_201_CREATED)
 def register_user(user: UserCreate, db: Session = Depends(get_db)):
     user = create_user(db, username=user.username, email=user.email, password=user.password)
     if not user:
@@ -28,4 +31,22 @@ def register_user(user: UserCreate, db: Session = Depends(get_db)):
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Email already registered"
         )
-    return user
+    access_token_expires = timedelta(minutes=60)
+    token = create_access_token(
+        data={"sub": user.email}, expires_delta=access_token_expires
+    )
+    return {"access_token": token}
+
+@router.post("/login", response_model=TokenResponse)
+def login_user(request: LoginRequest, db: Session = Depends(get_db)):
+    user = authenticate_user(db, email=request.email, password=request.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid credentials"
+        ) 
+    access_token_expires = timedelta(minutes=60)
+    access_token = create_access_token(
+        data={"sub": user.email}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token}
